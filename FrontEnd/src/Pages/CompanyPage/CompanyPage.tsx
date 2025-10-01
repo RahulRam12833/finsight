3f6af7f273e537ad37f2dbc76f68bae84e4e28fb
Rahul Ram <ramr12833@gmail.com>
2025-10-01 18:19:22 +0930

    Fetch company profile data

--- FrontEnd/src/Pages/CompanyPage/CompanyPage.tsx
+++ FrontEnd/src/Pages/CompanyPage/CompanyPage.tsx
@@ -1,9 +1,40 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import type { CompanyProfile } from "../../company";
+import { getCompanyProfile } from "../../api";
 
 interface Props {}
 
 const CompanyPage = (props: Props) => {
-  return <div>CompanyPage</div>;
-};
+  let symbol = window.location.pathname.split("/")[2];
+  const [company, setCompany] = useState<CompanyProfile>();
+  const [error, setError] = useState<string | null>(null);
 
+  useEffect(() => {
+    const fetchCompanyData = async () => {
+      try {
+        const data = await getCompanyProfile(symbol);
+        console.log(data[0]);
+        if (typeof data !== "string" && Array.isArray(data))
+          setCompany(data[0]);
+        else if (typeof data === "string") setError(data);
+      } catch (error) {
+        console.error("Error fetching company data:", error);
+      }
+    };
+    fetchCompanyData();
+  }, [symbol]);
+
+  return (
+    <>
+      {error && <h1>{error}</h1>}
+      {company && (
+        <div>
+          <h1 className="text-3xl font-bold">
+            {company.companyName} ({company.symbol})
+          </h1>
+        </div>
+      )}
+    </>
+  );
+};
 export default CompanyPage;
--- FrontEnd/src/api.tsx
+++ FrontEnd/src/api.tsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-import type { CompanySearch } from "./company";
+import type { CompanyProfile, CompanySearch } from "./company";
 
 interface SearchResult {
   data: CompanySearch[];
@@ -21,6 +21,29 @@ export const searchCompanies = async (query: string) => {
   } catch (error) {
     if (axios.isAxiosError(error)) {
       console.error("Error fetching company data:", error.message);
+      return error.message;
+    } else {
+      console.error("Unexpected error:", error);
+      return "An unexpected error has occurred";
+    }
+  }
+};
+
+export const getCompanyProfile = async (symbol: string) => {
+  try {
+    const response = await axios.get<CompanyProfile[]>(
+      "https://financialmodelingprep.com/stable/profile",
+      {
+        params: {
+          symbol, // symbol=AAPL
+          apikey: import.meta.env.VITE_FIN_API_KEY,
+        },
+      }
+    );
+    return response.data;
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      console.error("Error fetching company profile:", error.message);
       return error.message;
     } else {
       console.error("Unexpected error:", error);
