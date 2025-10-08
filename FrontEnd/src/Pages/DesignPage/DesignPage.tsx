5458e1be48ebe637754f31f0674d2a0640f08516
Rahul Ram <ramr12833@gmail.com>
2025-10-08 15:32:23 +1030

    Add a design page and table

---
+++ FrontEnd/src/Pages/DesignPage/DesignPage.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Table from "../../Components/Table/Table";
+
+type Props = {};
+
+const DesignPage = (props: Props) => {
+  return (
+    <>
+      <h1>FinSight Design Page</h1>
+      <h2>
+        Welcome to the design page of FinSight. This is where we will house
+        various design aspects of the app
+      </h2>
+      <Table />
+    </>
+  );
+};
+
+export default DesignPage;
