7c56ed0a53b5b2dc82e5a3db47f1991043c7936f
Rahul Ram <ramr12833@gmail.com>
2026-06-26 20:20:58 +0930

    Implement user registration, seed user roles, add migrations

--- BackEnd/FinSight.api/Data/ApplicationDBContext.cs
+++ BackEnd/FinSight.api/Data/ApplicationDBContext.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using FinSight.api.Models;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
 
 namespace FinSight.api.Data
 {
@@ -20,6 +21,25 @@ namespace FinSight.api.Data
         public DbSet<PlaceholderStock> PlaceholderStocks { get; set; }
         public DbSet<Stock> Stock { get; set; }
         public DbSet<Comment> Comments { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            List<IdentityRole> roles = new List<IdentityRole>
+            {
+                new IdentityRole
+                {
+                    Name="Admin",
+                    NormalizedName="ADMIN"
+                },
+                new IdentityRole
+                {
+                    Name="User",
+                    NormalizedName="USER"
+                },
+            };
+            builder.Entity<IdentityRole>().HasData(roles);
+        }
 
     }
 }
